43b837403c6de516323f07f4a22f17c58b2c52d7
jose <jgijalm388@g.educaand.es>
2026-07-01 11:45:03 +0200

    feat: add Vercel Analytics to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
+import { Analytics } from "@vercel/analytics/next"
 
 import App from './App.tsx'
 import './index.css'
@@ -9,6 +10,7 @@ import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
+    <Analytics />
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
